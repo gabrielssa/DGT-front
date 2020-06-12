92bb8a2293fae34e6b809f1b0a209b7ec642d74d
Gabriel Silva <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2020-06-12 16:14:42 -0300

    update

--- dgt-front/src/services/registerService.js
+++ dgt-front/src/services/registerService.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Axios from 'axios';
 
 const RegisterService = (props) =>{
-    Axios.post('https://dgt-back.herokuapp.com/api/user/register/', props).then( resp =>{
+    console.log("---- As props sendo enviadas ----");
+    console.log(props);
+    console.log("resultado ----")
+    Axios.post('https://dgt-back.herokuapp.com/api/user/register', props).then( resp =>{
         console.log(resp);
 
     }).catch(function(motivo){
